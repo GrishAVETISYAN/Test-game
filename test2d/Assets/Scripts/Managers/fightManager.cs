2a4fceccea9b8dc9b9c038fcf65d37401c7334ad
arturmart <artur.mart2002@gmail.com>
2020-07-19 03:23:06 +0400

    bayovki vra ashxatanq
    attackan avelacreci

--- test2d/Assets/Scripts/Managers/fightManager.cs
+++ test2d/Assets/Scripts/Managers/fightManager.cs
@@ -4,15 +4,96 @@ using UnityEngine;
 
 public class fightManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int actionMode = 0;
+    public bool lookMouseClk = true;
+
+    public float click = 0;
+
+    public float zamax = 0;
+    public float attack = 0;
+
+
+    public float[] zamaxAttackTime  = {0.1f, 1f};//poqric mec
+    public float[] attackTime       = {0.1f, 1f};
+    public int     attackCurrsor    = 0;
+
+    public float[] parirovanieTime = { 0.1f, 1f };//poqric mec
+    //public float[] attackTime = { 0.1f, 1f };
+
+    serializedAction SA;
+
     void Start()
     {
-        
+        SA = GetComponent<serializedAction>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        SA._doSerializedAction(actionMode);
+        if (Input.GetMouseButtonDown(0))
+        {
+            click = 0;
+            _doZamaxAttack(0);
+        }
+        if (Input.GetMouseButton(0))
+        {
+            click+= Time.deltaTime;
+            if(click> zamaxAttackTime[0] * 3 / 4)
+            {
+                _doZamaxAttackChangeToZamaxAttack(1);
+            }
+        }
         
+        _doCheckLoop();
+
+
+
+
+
+    }
+    void _doIdle()
+    {
+        
+        actionMode = 0;
+    }
+    void _doAttack()
+    {
+        actionMode = 2;
+    }
+    void _doZamaxAttack(int _attackCurrsor)
+    {
+        if (actionMode == 0)
+        {
+            attackCurrsor = _attackCurrsor;
+            attack = 0;
+            zamax = 0;
+            actionMode = 1;
+        }
+    }
+    
+    void _doZamaxAttackChangeToZamaxAttack(int _attackCurrsor)
+    {
+        if (actionMode == 1)attackCurrsor = _attackCurrsor;
+    }
+    void _doCheckLoop()
+    {
+        if (actionMode == 1)
+        {
+            if(zamax> zamaxAttackTime[attackCurrsor])
+            {
+                _doAttack();
+            }
+            zamax += Time.deltaTime;
+        }
+        if (actionMode == 2)
+        {
+            if (attack > attackTime[attackCurrsor])
+            {
+                _doIdle();
+            }
+            attack += Time.deltaTime;
+        }
+
     }
 }
---
+++ test2d/Assets/Scripts/serial/serializedAction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class serializedAction : MonoBehaviour
+{
+    dataBaseSprites Base;
+
+    Sprite stop;
+    Sprite zamax;
+    Sprite attack;
+    Sprite powerAttack;
+    
+
+
+    GameObject actionSerialized;
+    SpriteRenderer actionSerializedSR;
+    
+
+    void Start()
+    {
+        Base = Camera.main.GetComponent<dataBaseSprites>();
+        stop = Base.spriteArray[11];
+        zamax = Base.spriteArray[10];
+        attack = Base.spriteArray[7];
+        powerAttack = Base.spriteArray[9];
+
+        actionSerialized = new GameObject();
+        actionSerialized.name = "currsor";
+        actionSerializedSR = actionSerialized.AddComponent<SpriteRenderer>();
+
+        actionSerializedSR.sortingOrder = 1;
+    }
+
+    public void _doSerializedAction(int action)
+    {
+        actionSerialized.transform.position = gameObject.transform.position;
+
+        if (action==0)
+        {
+            actionSerializedSR.sprite = stop;
+            
+        }
+        if (action == 1)
+        {
+            actionSerializedSR.sprite = zamax;
+            
+        }
+        if (action ==2)
+        {
+            actionSerializedSR.sprite = attack;
+
+        }
+        actionSerialized.transform.position = new Vector2(transform.position.x, transform.position.y+1);
+
+
+
+
+    }
+}
