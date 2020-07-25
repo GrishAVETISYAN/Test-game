5a1ab0b7ac6a39f82b032f15afd63c87a99e1405
arturmart <artur.mart2002@gmail.com>
2020-07-25 20:33:56 +0400

    Zorder

---
+++ test-game3/Assets/Scripts/Other/ZorderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZorderScript : MonoBehaviour
+{
+    public int plus;
+    SpriteRenderer SR;
+    void Start()
+    {
+        SR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SR.sortingOrder = (int)((-transform.position.y+((float)plus)/96f)*96);
+    }
+}
+ 
