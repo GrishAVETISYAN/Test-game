ffeb2bdb5db2a89dcbec6037ad9006e84dd50a14
arturmart <artur.mart2002@gmail.com>
2020-07-30 01:54:11 +0400

    A stary hamarya prcnum em

--- test-game3/Assets/Scripts/Other/AStar.cs
+++ test-game3/Assets/Scripts/Other/AStar.cs
@@ -6,35 +6,147 @@ using UnityEngine;
 
 public class AStar : MonoBehaviour
 {
-    public bool[,] closes = new bool[,]{
+    public int lenX = 5;
+    public int lenY = 4;
+    public bool[,] closesBase = new bool[,]{
         { true, true , true, false, true },
         { true, true, true, false, true },
         { true, true, true, false, true },
         { true, true, true, true, true }};
 
-    //bool[,] check = new bool[closes.GetLength(0), closes.GetLength(1)];
-        
+    public int[,] sideStepBase = new int[,]{
+        { -1, -1 , -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 }};
 
-    Coords start_pos    = new Coords(0, 0);
+
+    public int[,] sideSideDistanceToTargetBase = new int[,]{
+        { -1, -1 , -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 }};
+
+    public int[,] sideSideWeightBase = new int[,]{
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 },
+        { -1, -1, -1, -1, -1 }};
+
+    //bool[,] check = new bool[closes.GetLength(0), closes.GetLength(1)];
+
+
+    Coords start_pos    = new Coords(1, 1);
     Coords target_pos   = new Coords(3, 3);
 
-
+    
     void Start()
     {
-        List<Coords> sideCoords = (get_sideCoords(new Coords(2,1)));
-        List<int> sideStep= (get_sideStep(new Coords(2, 1)));
-        List<int> sideDistanceToTarget = (get_sideDistanceToTarget(new Coords(2, 1)));
+        
+        List<Coords> sideCoords = (get_sideCoords(start_pos));
+        List<int> sideStep= (get_sideStep(start_pos));
+        List<int> sideDistanceToTarget = (get_sideDistanceToTarget(start_pos));
         List<int> sideWeight = (get_side_weigh(sideStep, sideDistanceToTarget));
 
+        changeSidestep(sideCoords, sideStep);
+        ChangeSideDistanceToTarget(sideCoords, sideDistanceToTarget);
+        ChangeSideSideWeight(sideCoords, sideWeight);
 
-        for (int i =0;i< sideCoords.Count;i++)
+        List<Coords> tt = ret_coord_by_weight(find_min_weight());
+
+        for (int i = 0; i < tt.Count; i++)
         {
-            Debug.Log(sideCoords[i].ToString() + " _ " + sideStep[i].ToString() + " _ " + sideDistanceToTarget[i].ToString() + " _ " + sideWeight[i].ToString() + " (" + i.ToString() + ")");
+            Debug.Log(tt[i].ToString());
         }
+
         
-       
+        sideCoords = (get_sideCoords(tt[0]));
+        sideStep = (get_sideStep(tt[0]));
+        sideDistanceToTarget = (get_sideDistanceToTarget(tt[0]));
+        sideWeight = (get_side_weigh(sideStep, sideDistanceToTarget));
+
+
+        changeSidestep(sideCoords, sideStep);
+        ChangeSideDistanceToTarget(sideCoords, sideDistanceToTarget);
+        ChangeSideSideWeight(sideCoords, sideWeight);
+
+
+
+
+
+
+        serializedWayFinderGird SWFG = GetComponent<serializedWayFinderGird>();
+        SWFG._Do_create_gird(new Vector2(0, 0), 1f, closesBase, start_pos.x, start_pos.y, target_pos.x, target_pos.y);
+        SWFG._Do_Text(closesBase, sideStepBase, sideSideDistanceToTargetBase, sideSideWeightBase);
+
+
+    }
+    int find_min_weight()
+    {
+        int min = -1;
+        for (int y = 0; y < lenY; y++)
+        {
+            for (int x = 0; x < lenX; x++)
+            {
+                if (sideSideWeightBase[y, x] != -1)
+                {
+                    if(min == -1 || min > sideSideWeightBase[y, x])
+                    {
+                        min = sideSideWeightBase[y, x];
+                    }
+                    
+                }
+            }
+        }
+        return (min);
+    }
+    List<Coords> ret_coord_by_weight(int sum)
+    {
+        List<Coords> ret = new List<Coords>();
+        for (int y = 0; y < lenY; y++)
+        {
+            for (int x = 0; x < lenX; x++)
+            {
+                if(sideSideWeightBase[y,x] == sum)
+                {
+                    ret.Add(new Coords(x,y));
+                }
+            }
+        }
+
+        return (ret);
+    }
+    void ChangeSideSideWeight(List<Coords> _coords, List<int> _sideWeight)
+    {
+        for (int i = 0; i < _coords.Count; i++)
+        {
+
+            sideSideWeightBase[_coords[i].y, _coords[i].x] = _sideWeight[i];
+
+
+        }
+    }
+    void changeSidestep(List<Coords> _coords, List<int> _sideSteps)
+    {
+        for (int i = 0; i < _coords.Count; i++)
+        {
+
+            sideStepBase[_coords[i].y, _coords[i].x] = _sideSteps[i];
+
+
+        }
     }
 
+    void ChangeSideDistanceToTarget(List<Coords> _coords, List<int> _sideDistanceToTarget)
+    {
+        for (int i = 0; i < _coords.Count; i++)
+        {
+
+            sideSideDistanceToTargetBase[_coords[i].y, _coords[i].x] = _sideDistanceToTarget[i];
+
+
+        }
+    }
     // Update is called once per frame
     void Update()
     {
@@ -54,10 +166,10 @@ public class AStar : MonoBehaviour
     {
         List<int> retCords = new List<int>();
 
-        Coords array_size = new Coords(closes.GetLength(1), closes.GetLength(0));
+        Coords array_size = new Coords(closesBase.GetLength(1), closesBase.GetLength(0));
         if (find_position.x < array_size.x - 1)
         {
-            if (closes[find_position.y, find_position.x + 1])
+            if (closesBase[find_position.y, find_position.x + 1])
             {
                 retCords.Add((target_pos.x - (find_position.x+1) + target_pos.y - (find_position.y))*10);
             }
@@ -65,28 +177,28 @@ public class AStar : MonoBehaviour
 
         if (find_position.x < array_size.x - 1 && find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x + 1])
+            if (closesBase[find_position.y - 1, find_position.x + 1])
             {
                 retCords.Add((target_pos.x - (find_position.x + 1) + target_pos.y - (find_position.y-1)) * 10);
             }
         }
         if (find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x])
+            if (closesBase[find_position.y - 1, find_position.x])
             {
                 retCords.Add((target_pos.x - (find_position.x) + target_pos.y - (find_position.y-1)) * 10);
             }
         }
         if (find_position.x > 0 && find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x - 1])
+            if (closesBase[find_position.y - 1, find_position.x - 1])
             {
                 retCords.Add((target_pos.x - (find_position.x - 1) + target_pos.y - (find_position.y-1)) * 10);
             }
         }
         if (find_position.x > 0)
         {
-            if (closes[find_position.y, find_position.x - 1])
+            if (closesBase[find_position.y, find_position.x - 1])
             {
                 retCords.Add((target_pos.x - (find_position.x - 1) + target_pos.y - (find_position.y)) * 10);
             }
@@ -94,7 +206,7 @@ public class AStar : MonoBehaviour
 
         if (find_position.x > 0 && find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x - 1])
+            if (closesBase[find_position.y + 1, find_position.x - 1])
             {
                 retCords.Add((target_pos.x - (find_position.x - 1) + target_pos.y - (find_position.y+1)) * 10);
             }
@@ -102,14 +214,14 @@ public class AStar : MonoBehaviour
 
         if (find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x])
+            if (closesBase[find_position.y + 1, find_position.x])
             {
                 retCords.Add((target_pos.x - (find_position.x) + target_pos.y - (find_position.y+1)) * 10);
             }
         }
         if (find_position.x < array_size.x - 1 && find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x + 1])
+            if (closesBase[find_position.y + 1, find_position.x + 1])
             {
                 retCords.Add((target_pos.x - (find_position.x + 1) + target_pos.y - (find_position.y+1)) * 10);
             }
@@ -120,10 +232,10 @@ public class AStar : MonoBehaviour
     {
         List<int> retCords = new List<int>();
 
-        Coords array_size = new Coords(closes.GetLength(1), closes.GetLength(0));
+        Coords array_size = new Coords(closesBase.GetLength(1), closesBase.GetLength(0));
         if (find_position.x < array_size.x - 1)
         {
-            if (closes[find_position.y, find_position.x + 1])
+            if (closesBase[find_position.y, find_position.x + 1])
             {
                 retCords.Add(10);
             }
@@ -131,28 +243,28 @@ public class AStar : MonoBehaviour
 
         if (find_position.x < array_size.x - 1 && find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x + 1])
+            if (closesBase[find_position.y - 1, find_position.x + 1])
             {
                 retCords.Add(14);
             }
         }
         if (find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x])
+            if (closesBase[find_position.y - 1, find_position.x])
             {
                 retCords.Add(10);
             }
         }
         if (find_position.x > 0 && find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x - 1])
+            if (closesBase[find_position.y - 1, find_position.x - 1])
             {
                 retCords.Add(14);
             }
         }
         if (find_position.x > 0)
         {
-            if (closes[find_position.y, find_position.x - 1])
+            if (closesBase[find_position.y, find_position.x - 1])
             {
                 retCords.Add(10);
             }
@@ -160,7 +272,7 @@ public class AStar : MonoBehaviour
 
         if (find_position.x > 0 && find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x - 1])
+            if (closesBase[find_position.y + 1, find_position.x - 1])
             {
                 retCords.Add(14);
             }
@@ -168,14 +280,14 @@ public class AStar : MonoBehaviour
 
         if (find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x])
+            if (closesBase[find_position.y + 1, find_position.x])
             {
                 retCords.Add(10);
             }
         }
         if (find_position.x < array_size.x - 1 && find_position.y < array_size.y - 1)
         {
-            if (closes[find_position.y + 1, find_position.x + 1])
+            if (closesBase[find_position.y + 1, find_position.x + 1])
             {
                 retCords.Add(14);
             }
@@ -186,10 +298,10 @@ public class AStar : MonoBehaviour
     {
         List<Coords> retCords = new List<Coords>();
 
-        Coords array_size= new Coords(closes.GetLength(1), closes.GetLength(0));
+        Coords array_size= new Coords(closesBase.GetLength(1), closesBase.GetLength(0));
         if (find_position.x < array_size.x-1)
         {
-            if(closes[find_position.y, find_position.x + 1])
+            if(closesBase[find_position.y, find_position.x + 1])
             {
                 retCords.Add(new Coords(find_position.x + 1, find_position.y));
             }
@@ -197,28 +309,28 @@ public class AStar : MonoBehaviour
 
         if (find_position.x < array_size.x-1 && find_position.y >0)
         {
-            if (closes[find_position.y-1, find_position.x + 1])
+            if (closesBase[find_position.y-1, find_position.x + 1])
             {
                 retCords.Add(new Coords(find_position.x + 1, find_position.y-1));
             }
         }
         if (find_position.y > 0)
         {
-            if (closes[find_position.y - 1, find_position.x])
+            if (closesBase[find_position.y - 1, find_position.x])
             {
                 retCords.Add(new Coords(find_position.x, find_position.y-1));
             }
         }
         if (find_position.x >0 && find_position.y > 0)
         {
-            if (closes[find_position.y-1, find_position.x -1])
+            if (closesBase[find_position.y-1, find_position.x -1])
             {
                 retCords.Add(new Coords(find_position.x -1, find_position.y-1));
             }
         }
         if (find_position.x > 0 )
         {
-            if (closes[find_position.y, find_position.x - 1])
+            if (closesBase[find_position.y, find_position.x - 1])
             {
                 retCords.Add(new Coords(find_position.x - 1, find_position.y));
             }
@@ -226,7 +338,7 @@ public class AStar : MonoBehaviour
       
         if (find_position.x > 0 && find_position.y < array_size.y-1)
         {
-            if (closes[find_position.y+1, find_position.x - 1])
+            if (closesBase[find_position.y+1, find_position.x - 1])
             {
                 retCords.Add(new Coords(find_position.x - 1, find_position.y+1));
             }
@@ -234,14 +346,14 @@ public class AStar : MonoBehaviour
 
         if (find_position.y < array_size.y-1)
         {
-            if (closes[find_position.y + 1, find_position.x])
+            if (closesBase[find_position.y + 1, find_position.x])
             {
                 retCords.Add(new Coords(find_position.x, find_position.y+1));
             }
         }
         if (find_position.x < array_size.x-1 && find_position.y < array_size.y-1)
         {
-            if (closes[find_position.y + 1, find_position.x+1])
+            if (closesBase[find_position.y + 1, find_position.x+1])
             {
                 retCords.Add(new Coords(find_position.x+1, find_position.y+1));
             }
@@ -262,4 +374,6 @@ public class AStar : MonoBehaviour
 
         public override string ToString() => $"({x}, {y})";
     }
+
+    
 }
--- test-game3/Assets/Scripts/serial/serializedWayFinderGird.cs
+++ test-game3/Assets/Scripts/serial/serializedWayFinderGird.cs
@@ -1,46 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class serializedWayFinderGird : MonoBehaviour
 {
-    
-    public GameObject txt;
 
-    public bool[,] closes = new bool[,]{
-        { true, true , true, true, true },
-        { true, true, false, false, true },
-        { true, true, true, false, true },
-        { true, true, true, true, true }};
+    public GameObject canvas;
+    public Font fnt;
 
-    Vector2 zero_position = new Vector2(0, 0);
-    float cells_distance = 1f;
+    Text[,] texts;
+
+    //Vector2 zero_position = new Vector2(0, 0);
+    //float cells_distance = 1f;
 
     GameObject mainGird;
-    void Start()
+    
+
+
+    public void _Do_Text(bool[,] closes, int[,]  sideStep, int[,]  sideDistanceToTarget, int[,] sideSideWeight)
     {
-        
-        
-        _Do_create_gird( zero_position, closes.GetLength(1), closes.GetLength(0),  cells_distance);
+        int len_x = closes.GetLength(1);
+        int len_y = closes.GetLength(0);
+
+        for (int y = 0; y < len_y; y++)
+        {
+            for (int x = 0; x < len_x; x++)
+            {
+                Text txt = texts[y, x];
+                txt.text = "<color=white>("+x.ToString()+"," + y.ToString()+ ")</color>";
+                if(closes[y, x])    txt.text += "<color=green> True </color>";
+                else                txt.text += "<color=red> False </color>";
+                if (sideStep[y, x] != -1) txt.text += "\n<color=yellow>" + sideStep[y, x].ToString() + "</color>";
+                if (sideDistanceToTarget[y, x] != -1) txt.text += "    <color=blue>" + sideDistanceToTarget[y, x].ToString() + "</color>";
+                if (sideSideWeight[y, x] != -1) txt.text += "    <color=red>" + sideSideWeight[y, x].ToString() + "</color>";
+            }
+        }
     }
-
-   
-
-    void _Do_create_gird(Vector2 zero_position, int len_x, int len_y, float cells_distance)
+    public void _Do_create_gird(Vector2 zero_position, float cells_distance, bool[,] closes, int _start_pos_x, int _start_pos_y, int _target_pos_x, int _target_pos_y)
     {
+        
         dataBaseSprites Base;
         Sprite cube;
         Sprite cube_close;
+        Sprite cube_start;
+        Sprite cube_target;
 
         Base = Camera.main.GetComponent<dataBaseSprites>();
         cube = Base.spriteArray[12];
         cube_close = Base.spriteArray[13];
+        cube_start = Base.spriteArray[14];
+        cube_target = Base.spriteArray[15];
 
 
         mainGird = new GameObject();
         mainGird.name = "mainGird";
+        int len_x = closes.GetLength(1);
+        int len_y = closes.GetLength(0);
+        texts = new Text[len_y, len_x];
 
-        for(int y = 0;y< len_y; y++)
+        for (int y = 0;y< len_y; y++)
         {
             for (int x = 0; x < len_x; x++)
             {
@@ -54,19 +73,62 @@ public class serializedWayFinderGird : MonoBehaviour
                 ZS.plus = -96/2;
                 GirdCell.transform.position = new Vector2(x* cells_distance,y* cells_distance);
                 GirdCell.transform.parent = mainGird.transform;
-                if (closes[y,x]) GirdCellSR.sprite = cube;
-                else GirdCellSR.sprite = cube_close;
+                if (x == _start_pos_x && y == _start_pos_y)
+                {
+                    GirdCellSR.sprite = cube_start;
+                }
+                else if (x == _target_pos_x && y == _target_pos_y)
+                {
+                    GirdCellSR.sprite = cube_target;
+                }
+                else
+                {
+                    if (closes[y, x]) GirdCellSR.sprite = cube;
+                    else GirdCellSR.sprite = cube_close;
+                }
+
+                GameObject text = CreateText(canvas.transform, x, y, Color.white);
+                texts[y, x] = text.GetComponent<Text>();
 
 
-                GameObject txtObj = Instantiate(txt);
-                txtObj.transform.position = new Vector2(x * cells_distance, y * cells_distance);
-                
-                txtObj.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
 
             }
 
         }
 
         
+    }
+    GameObject CreateText(Transform canvas_transform, float x, float y, Color text_color)
+    {
+        GameObject UItextGO = new GameObject("Text");
+        
+
+        RectTransform trans = UItextGO.AddComponent<RectTransform>();
+        trans.anchoredPosition = new Vector2(x, y);
+
+        Text text = UItextGO.AddComponent<Text>();
+        text.text = "txt";
+        text.fontSize = 16;
+        text.color = text_color;
+        UItextGO.transform.SetParent(canvas_transform);
+        text.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        text.font = fnt;
+        
+
+        return UItextGO;
+    }
+
+    public struct Coords
+    {
+        public Coords(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        public int x { get; }
+        public int y { get; }
+
+        public override string ToString() => $"({x}, {y})";
     }
 }
