9242ac6e7379014b3c6bcc1eff650190be5d44c2
Grish <Grish.avetisyan2003@gmail.com>
2020-08-03 21:40:16 +0400

    Removed debug.log and added comments.

--- test-game3/Assets/Scripts/char/charMatChange.cs
+++ test-game3/Assets/Scripts/char/charMatChange.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class charMatChange : MonoBehaviour
 {
-    public void _doChangeMat (Material mat, string name)
+    public void _doChangeMat (Material mat, string name) //replaces the material of the target object with the given one.
     {
         GameObject targetObj = GameObject.Find(name);
         targetObj.GetComponent<Renderer>().material = mat;
-        Debug.Log(targetObj.GetComponent<Renderer>().material);
     }
 }
