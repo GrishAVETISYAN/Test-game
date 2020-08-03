9cc68d43e07be952b1b2938a5703251eee0d0110
Grish <Grish.avetisyan2003@gmail.com>
2020-08-03 21:37:41 +0400

    Added material changing code.

---
+++ test-game3/Assets/Scripts/char/charMatChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charMatChange : MonoBehaviour
+{
+    public void _doChangeMat (Material mat, string name)
+    {
+        GameObject targetObj = GameObject.Find(name);
+        targetObj.GetComponent<Renderer>().material = mat;
+        Debug.Log(targetObj.GetComponent<Renderer>().material);
+    }
+}
