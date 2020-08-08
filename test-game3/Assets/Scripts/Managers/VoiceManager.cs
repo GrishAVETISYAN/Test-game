554be22625fe63caf654d06e1c56e81e30ea83f6
Grish <Grish.avetisyan2003@gmail.com>
2020-08-08 19:38:19 +0400

    Finished making VoiceManager, also changed Bob.

---
+++ test-game3/Assets/Scripts/Managers/VoiceManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceManager : MonoBehaviour
+{
+    [SerializeField] private string text;
+    [SerializeField] private float delay;
+    [SerializeField] private float pitch;
+    [SerializeField] private float long_delay;
+    SoundPlayer SP;
+
+    void Start()
+    {
+       SP = GetComponent<SoundPlayer>();
+       StartCoroutine(SP._doPlayVoice(text, delay, long_delay, pitch));
+       
+    }
+}
+
---
+++ test-game3/Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{   
+    [SerializeField] AudioSource source;
+    
+    [SerializeField] AudioClip sound;
+    public IEnumerator _doPlayVoice(string text, float delay, float long_delay, float pitch)
+    {
+        string [] ts = text.Split(' '); 
+
+        foreach(string st in ts )
+        {
+            for(int i = 0; i < st.Length; i++)
+            {
+                source.pitch = pitch + Random.Range(-0.05f, 0.05f);
+                yield return new WaitForSeconds(delay);
+                source.PlayOneShot(sound);
+            }
+
+            yield return new WaitForSeconds(long_delay);
+        }
+    } 
+}
